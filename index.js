32df6d5a586701b8b98c85ed54a99fdd5df1b387
Harriet Ryder <harriet@zuko.io>
2018-11-23 13:36:39 +0000

    Begin making an endpoint for running docker on remote host

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const fs = require('fs')
+const path = require('path')
+const SshClient = require('node-ssh')
+const ssh = new SshClient();
 const challenges = require('./data/challenges.json');
 const solutionsController = require('./controllers/solutions.controller');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+const REMOTE_HOST = '178.62.51.68';
+
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -22,6 +29,38 @@ app.get('/challenges/:id', (req, res) => {
 });
 
 app.post('/solutions/:id', solutionsController.validate);
+
+app.post('/remote/:id', (req, res) => {
+  if (isNaN(Number(req.params.id))) {
+    return res.redirect('/');
+  }
+
+  const challengeId = Number(req.params.id);
+
+  ssh.connect({
+    host: REMOTE_HOST,
+    username: 'root',
+    privateKey: '/Users/harrietryder/.ssh/id_rsa'
+  }).then(() => {
+    console.log(`Connected to ${REMOTE_HOST}`);
+    return ssh.execCommand(`docker run -i test_runner ${challengeId}`, {
+      stdin: req.body.code,
+    })
+  })
+  .then(sshRes => {
+    console.log('Response received from remote host', {sshRes});
+
+    if (sshRes.code === 0) return res.status(200).send('Pass');
+    else return res.status(400).send('Fail');
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).send({err});
+  })
+  .then(() => {
+    return ssh.dispose();
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
