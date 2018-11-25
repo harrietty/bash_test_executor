757206e552ec632164cd2c162ba73cc5f3e02194
Harriet Ryder <harriethryder@gmail.com>
2018-11-25 08:57:34 +0000

    Style challenges page

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/practice', (req, res) => {
+app.get('/challenges/all', (req, res) => {
   res.render('challenges', {challenges});
 });
 
